353049c6a433a7eabc00763c1b1719de402b43bf
Andriy-Lik <alan321987@gmail.com>
2023-08-23 18:10:28 +0300

    delete helpers

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,5 +1,6 @@
-const { listContacts, getContactById, removeContact, addContact, updateContact, } = require("../../models/contacts");
 const express = require('express');
+
+const { listContacts, getContactById, removeContact, addContact, updateContact, } = require("../../models/contacts");
 
 const router = express.Router();
 
@@ -7,19 +8,23 @@ router.get('/', async (req, res, next) => {
   try {
     const result = await listContacts();
     res.status(200).json(result);
-  } catch (err) {
-    res.status(500).json({ message: "Server error" });
+  } catch (error) {
+    next(error);
   }
 })
 
 router.get('/:contactId', async (req, res, next) => {
-
-  const { contactId } = req.params;
-  const result = await getContactById(contactId);
-  if (!result) {
-    return res.status(404).json({ message: "Not found", status: "error" });
-  } 
-  res.json(result);
+  try {
+    const { contactId } = req.params;
+    const result = await getContactById(contactId);
+    if (!result) {
+      return res.status(404).json({ message: "Not found"});
+    } 
+    res.status(200).json(result);
+  } catch (error) {
+    next(error);
+  }
+  
 })
 
 router.post('/', async (req, res, next) => {
