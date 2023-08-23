a6d9e595445f8d15528c93fa9bf0449568fc5806
Andriy-Lik <alan321987@gmail.com>
2023-08-23 17:21:09 +0300

    added folder helpers

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,13 +1,25 @@
-const express = require('express')
+const { listContacts, getContactById, removeContact, addContact, updateContact, } = require("../../models/contacts");
+const express = require('express');
 
-const router = express.Router()
+const router = express.Router();
 
 router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  try {
+    const result = await listContacts();
+    res.status(200).json(result);
+  } catch (err) {
+    res.status(500).json({ message: "Server error" });
+  }
 })
 
 router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
+
+  const { contactId } = req.params;
+  const result = await getContactById(contactId);
+  if (!result) {
+    return res.status(404).json({ message: "Not found", status: "error" });
+  } 
+  res.json(result);
 })
 
 router.post('/', async (req, res, next) => {
@@ -22,4 +34,4 @@ router.put('/:contactId', async (req, res, next) => {
   res.json({ message: 'template message' })
 })
 
-module.exports = router
+module.exports = router;
