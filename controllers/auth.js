f00f3d63e9f984031a1370c3fb915b50b5ee7513
Andriy-Lik <alan321987@gmail.com>
2023-09-01 23:33:25 +0300

    hw 04

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,14 +1,53 @@
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
 const {User} = require("../models");
 const { ctrlWrapper } = require("../helpers");
 
+const {SECRET_KEY} = process.env;
+
 const register = async(req, res) => {
-    const newUser = await User.create(req.body);
+    const {email, password} = req.body;
+    const user = await User.findOne({email});
+
+    if(user) {
+        return res.status(409).json({ message: "Email in use"});
+    }
+
+    const hashPassword = await bcrypt.hash(password, 10);
+
+    const newUser = await User.create({...req.body, password: hashPassword});
+    
     res.status(201).json({
         email: newUser.email,
         subscription: newUser.subscription,
     })
 };
 
+const login = async(req, res) => {
+    const {email, password} = req.body;
+    const user = await User.findOne({email});
+    if (!user) {
+        return res.status(401).json({ message: "Email or password is wrong"});  
+    }
+    const passwordCompare = await bcrypt.compare(password, user.password);
+    if (!passwordCompare) {
+        return res.status(401).json({ message: "Email or password is wrong"});
+    }
+    const payload = {
+        id: user._id,
+    };
+    const token = jwt.sign(payload, SECRET_KEY, {expiresIn: "23h"});
+    res.status(200).json({
+        token,
+        user: {
+            email: user.email,
+            subscription: user.subscription,
+        },
+    })
+};
+
 module.exports = {
     register: ctrlWrapper(register),
+    login: ctrlWrapper(login),
 };
--- routes/api/auth.js
+++ routes/api/auth.js
@@ -2,12 +2,15 @@ const express = require("express");
 
 const ctrl = require("../../controllers/auth");
 const {validateBody} = require("../../middlewares");
-const {registerSchema} = require("../../schemas");
+const {registerSchema, loginSchema} = require("../../schemas");
 
 const router = express.Router();
 
 
-// registration
+// signup
 router.post("/register", validateBody(registerSchema), ctrl.register);
+
+// signin
+router.post("/login", validateBody(loginSchema), ctrl.login);
 
 module.exports = router;
