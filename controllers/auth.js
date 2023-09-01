f260ca6996f77bf2bb0acb856dc0ada612a06619
Andriy-Lik <alan321987@gmail.com>
2023-09-01 17:46:41 +0300

    change files

--- controllers/auth.js
+++ controllers/auth.js
@@ -5,6 +5,7 @@ const register = async(req, res) => {
     const newUser = await User.create(req.body);
     res.status(201).json({
         email: newUser.email,
+        subscription: newUser.subscription,
     })
 };
 
