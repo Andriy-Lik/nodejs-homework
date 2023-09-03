07e30e9d3b94c484a3f5c49d381fc62e6c798cdb
Andriy-Lik <alan321987@gmail.com>
2023-09-03 21:22:36 +0300

    HW 04

--- middlewares/validateFavorite.js
+++ middlewares/validateFavorite.js
@@ -1,11 +1,11 @@
 const validateFavorite = schema => {
     const func = (req, res, next) => {
         if (Object.keys(req.body).length === 0) {
-            return res.status(400).json({ message: "missing field favorite"});
+            return res.status(400).json({message: "missing field favorite"});
         } else {
             const { error } = schema.validate(req.body);
             if (error) {
-                return res.status(400).json({ message: error.message});
+                return res.status(400).json({message: error.message});
             }
             next()
         }
--- routes/api/auth.js
+++ routes/api/auth.js
@@ -7,10 +7,8 @@ const {registerSchema, loginSchema} = require("../../schemas");
 const router = express.Router();
 
 
-// signup
 router.post("/register", validateBody(registerSchema), ctrl.register);
 
-// signin
 router.post("/login", validateBody(loginSchema), ctrl.login);
 
 router.get("/current", authenticate, ctrl.getCurrent);
