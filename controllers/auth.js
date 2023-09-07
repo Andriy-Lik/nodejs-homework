f4126d18bcb17f1e79c11a7d85f7c299e2d40dbe
Andriy-Lik <alan321987@gmail.com>
2023-09-07 23:11:11 +0300

    HW 05

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,10 +1,16 @@
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
+const gravatar = require("gravatar");
+const path = require("path");
+const fs = require("fs/promises");
+const Jimp = require("jimp");
 
 const {User} = require("../models");
 const { ctrlWrapper } = require("../helpers");
 
 const {SECRET_KEY} = process.env;
+
+const avatarsDir = path.join(__dirname, "../", "public", "avatars");
 
 const register = async(req, res) => {
     const {email, password} = req.body;
@@ -15,8 +21,9 @@ const register = async(req, res) => {
     }
 
     const hashPassword = await bcrypt.hash(password, 10);
+    const avatarURL = gravatar.url(email);
 
-    const newUser = await User.create({...req.body, password: hashPassword});
+    const newUser = await User.create({...req.body, password: hashPassword, avatarURL});
     
     res.status(201).json({
         user: {
@@ -64,9 +71,34 @@ const logout = async (req, res) => {
     res.status(204).json({message: "Logout success"});
 };
 
+const updateAvatar = async (req, res) => {
+    const {_id} = req.user;
+    const {path: tempUpload, originalname} = req.file;
+    Jimp.read(tempUpload)
+        .then((img) => {
+            return img
+                .resize(250, 250)
+                .write(resultUpload);
+        })
+        .catch((err) => {
+            console.error(err);
+        });
+
+    const filename = `${_id}_${originalname}`;
+    const resultUpload = path.join(avatarsDir, filename);
+    await fs.rename(tempUpload, resultUpload);
+    const avatarURL = path.join("avatars", filename);
+    await User.findByIdAndUpdate(_id, {avatarURL});
+
+    res.json({
+        avatarURL,
+    });
+};
+
 module.exports = {
     register: ctrlWrapper(register),
     login: ctrlWrapper(login),
     getCurrent: ctrlWrapper(getCurrent),
     logout: ctrlWrapper(logout),
+    updateAvatar: ctrlWrapper(updateAvatar),
 };
