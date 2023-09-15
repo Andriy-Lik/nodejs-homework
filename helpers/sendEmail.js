38fae1619f0d364d2602b7fd9f476f79df968606
Andriy-Lik <alan321987@gmail.com>
2023-09-15 19:48:24 +0300

    update sendEmail.js

--- helpers/sendEmail.js
+++ helpers/sendEmail.js
@@ -22,32 +22,3 @@ const sendEmail = async (data) => {
 };
 
 module.exports = sendEmail;
-
-// const email = {
-//     to: "123",
-//     from: "alan321@meta.ua",
-//     subject: "Test email",
-//     html: "<p><strong>Test email</strong> from localhost:3000</p>"
-// };
-
-// transport.sendMail(email)
-//     .then(() => console.log("Email send success"))
-//     .catch(error => console.log(error.message));
-
-
-
-// sendgrid
-// const sgMail = require("@sendgrid/mail");
-// require("dotenv").config();
-
-// const {SENDGRID_API_KEY} = process.env;
-
-// sgMail.setApiKey(SENDGRID_API_KEY);
-
-// const sendEmail = async (data) => {
-//     const email = {...data, from: "alan321987@gmail.com"};
-//     await sgMail.send(email);
-//     return true;
-// };
-
-// module.exports = sendEmail;
